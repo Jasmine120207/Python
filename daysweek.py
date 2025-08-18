84e35dd99f60368040c7b24e3f0e5b9a7eec8853
Jasmine120207 <jasu67524@gmail.com>
2025-08-18 22:56:07 +0530

    Create daysweek.py

---
+++ daysweek.py
@@ -0,0 +1,13 @@
+n=int(input("enter the number"))
+if(n==1):
+  print("Monday")
+elif(n==2):
+  print("Tuesady")
+elif(n==3):
+  print("Wednesday")
+elif(n==4):
+  print("Thursday")
+elif(n==5):
+  print("Friday")
+else:
+  print("Saturday")
