ca6669379e59ec3792f60a1e196c70f9b1125dd5
Jasmine120207 <jasu67524@gmail.com>
2025-08-21 16:54:41 +0530

    Update fibonaci.py

--- fibonaci.py
+++ fibonaci.py
@@ -1,8 +1,9 @@
 n=int(input("enter the number"))
 fib=0
 fib1=1
-for i in range(1,n+1):
+for i in range(n):
   fib2=fib+fib1
-  fib1=fib
-  fib2=fib1
-print(fib,fib1,fib2)
+  print(fib)
+  fib=fib1
+  fib1=fib2
+
